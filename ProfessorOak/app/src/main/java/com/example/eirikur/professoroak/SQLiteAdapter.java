c007eef54c96f4c1ecf62d186d3d347833ef9205
TanjaRio <tanjario@hotmail.no>
2016-05-26 15:50:30 +0200

    database update

---
+++ ProfessorOak/app/src/main/java/com/example/eirikur/professoroak/SQLiteAdapter.java
@@ -0,0 +1,92 @@
+package com.example.eirikur.professoroak;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Bruker on 26.05.2016.
+ */
+public class SQLiteAdapter {
+    public static final String DATABASE_NAME = "Pokemons";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME="pokemon";
+
+    public static final String POKEMON_ID = "id";
+    public static final String POKEMON_NAME = "name";
+    public static final String POKEMON_LAT = "lat";
+    public static final String POKEMON_LNG = "lng";
+
+    public static final String PREFERENCES_DB = "PreferencesDb";
+
+    private SQLiteHelper databaseHelper;
+    private SQLiteDatabase sqLiteDatabase;
+    private Context context;
+
+    private SharedPreferences preferences;
+
+    private long id = 1;
+
+    public SQLiteAdapter (Context context) {
+        this.context = context;
+
+        preferences = context.getSharedPreferences(PREFERENCES_DB, Activity.MODE_APPEND);
+        if(preferences != null){
+            id = preferences.getLong("nextId", 1);
+        }
+
+    }
+
+    public SQLiteAdapter open () {
+        databaseHelper = new SQLiteHelper(context,DATABASE_NAME,null,DATABASE_VERSION);
+        sqLiteDatabase = databaseHelper.getWritableDatabase(); //Should be in a background thread, i.e. Async
+        return this;
+    }
+
+    public long create(Pokemon pokemon) {
+        ContentValues values = new ContentValues();
+        values.put(POKEMON_ID,id);
+        values.put(POKEMON_NAME, pokemon.getName());
+        values.put(POKEMON_LAT, pokemon.getLat());
+        values.put(POKEMON_LNG, pokemon.getLng());
+
+        sqLiteDatabase.insert(TABLE_NAME,null,values);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putLong("nextId",id+1);
+        editor.commit();
+
+        return id++;
+
+
+    }
+
+    public Cursor readAll() {
+        String[] columns = new String[]{POKEMON_ID, POKEMON_NAME, POKEMON_LAT, POKEMON_LNG};
+        return sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        //return sqLiteDatabase.query(TABLE_NAME, columns, null, null, null, null, null);
+        //(Table_name, columns, where, where args, group by, order by, having)
+    }
+
+    public boolean update(Long pokemonId, Pokemon pokemon) {
+        ContentValues values = new ContentValues();
+        values.put(POKEMON_NAME, pokemon.getName());
+        //values.put(PERSON_AGE, person.getAge());
+
+        String whereClause = POKEMON_ID + " = ?";
+        //String[] whereArgs = new String[]{personId.toString()};
+
+        //int numberOfRowsUpdated = sqLiteDatabase.update(TABLE_NAME, values, whereClause, whereArgs);
+
+        //return (numberOfRowsUpdated == 1);
+        return false;
+    }
+
+
+
+    public void close() {
+        sqLiteDatabase.close();
+    }
+}
---
+++ ProfessorOak/app/src/main/java/com/example/eirikur/professoroak/SQLiteHelper.java
@@ -0,0 +1,30 @@
+package com.example.eirikur.professoroak;
+import com.example.eirikur.*;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Bruker on 26.05.2016.
+ */
+public class SQLiteHelper extends SQLiteOpenHelper{
+    private static final String SCRIPT_CREATE_DATABASE = "create table pokemon ("
+            + "id integer primary key autoincrement, "
+            + "name text not null, "
+            + "lat real not null, "
+            + "lng real not null);";
+
+    public SQLiteHelper (Context context, String name, SQLiteDatabase.CursorFactory factory, int version ) {
+        super(context,name,factory,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SCRIPT_CREATE_DATABASE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
